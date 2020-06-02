fbd41cdc00beedafbd4e7091118f6dad76ca17d2
MJ-SEO <21600343@handong.edu>
2020-06-02 14:51:17 +0900

    good

--- lab_7/main.c
+++ lab_7/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef 
 	struct _node_t {
@@ -16,7 +17,8 @@ tree_create_node (char* data)
 {
 	node_t * root ;
 	root = (node_t *) malloc(sizeof(node_t)) ;
-	root->data = data ;
+	root->data = (char*) malloc(sizeof(char)*64);
+	strcpy(root->data, data);
 	root->child = 0x0 ;
 	root->sibling = 0x0 ; 
 	return root ;
@@ -53,7 +55,7 @@ tree_height(tree_t * t)
 node_t *
 tree_search (node_t * t, char* key)
 {
-	if (t->data == key)
+	if (strcmp(t->data,key))
 		return t ;
 
 	for (node_t * i = t->child ; i != 0x0 ; i = i->sibling) {
@@ -71,6 +73,10 @@ _tree_print (node_t * t, int level)
 {
 	for (int i = 0 ; i < level ; i++)
 		printf("  ") ;
+
+	if(level>0)
+		printf("+--");
+
 	printf("%s\n", t->data) ;
 	
 	node_t * curr ;
@@ -84,14 +90,15 @@ tree_print (tree_t * t)
 	_tree_print(t, 0) ;
 }
 
-/*
+
 tree_t *
 read_a_tree(char *s)
 {
 	char * begin, * end, * sub ;
 	int n ;
-	char* data ;	
+	int data ;	
 
+	data = atoi(s) ;
 	node_t * t = tree_create_node(data) ;
 
 	for (begin = s + 1 ; *begin != '(' && *begin != ')' && *begin != ',' ; begin++) ;
@@ -116,24 +123,88 @@ read_a_tree(char *s)
 
 	return t ;
 }
-*/
+
 int
 main ()
 {
-	int man, rel;
+    tree_t *t[100] = {0};
+    char store[100][64];
 
-	printf("사람 수, 관계 수: ");
-	scanf("%d, %d", &man, &rel);
-	
-		
+    int man, rel;
+    char s1[64];
+    char s2[64];
+    int n=0;
 
-	node_t arr[man];
-	
+    int child[100] = {0};
 
-	for(int i=0; i<man; i++){
-		arr[i] = 
+//    printf("사람 수, 관계 수: ");
+    scanf("%d %d", &man, &rel);
+//    printf("DEBUG: man: %d rel: %d\n", man, rel);
+
+    for (int i = 0; i < rel; i++){
+//	printf("스승, 제자:");
+        scanf("%s %s", s1, s2);
+        int a = 0, b = 0;
+        tree_t *t1;
+        tree_t *t2;
+        for (a = 0; a < n; a++) {
+            if (strcmp(store[a], s1) == 0)
+                break;
+        }
+        
+	if (a >= n){
+            t1 = tree_create_node(s1);
+            strcpy(store[a], s1);
+            t[a] = t1;
+            n++;
+        }
+        else{
+            t1 = t[a];
+        }
+
+	for (b = 0; b < n; b++){
+            if (strcmp(store[b], s2) == 0)
+                break;
+        }
+        
+	if (b >= n){
+            t2 = tree_create_node(s2);
+            strcpy(store[b], s2);
+            t[b] = t2;
+            n++;
+        }
+        else{
+            t2 = t[b];
 	}
-	
 
-	return 0 ;
+    	    child[b] = 1;
+            tree_add_child(t1, t2);
+   
+    }
+
+
+    for (int i = 0; i < man; i++){
+        if (!child[i])
+            tree_print(t[i]);
+    }
+
+return 0 ;
 }
+/*
+	tree_t * t ;
+	t = tree_create_node("mj");
+
+
+		printf("입력: ");
+		scanf("%s", s);
+		printf("DEBUG: %s\n", s);
+		node_t* e = tree_create_node(s); 
+		tree_add_child(t,e);	
+
+		printf("입력: ");
+		scanf("%s", s2);
+		printf("DEBUG: %s\n", s2);
+		node_t* d = tree_create_node(s2); 
+		tree_add_child(e,d);	
+*/
+
